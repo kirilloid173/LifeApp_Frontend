3df53ce786e8988d785cf62a54729412c5f942a5
Amogus <empty>
2025-09-17 16:16:12 +0400

    3.3-send_messages_by_press_enter

--- src/components/WriteMessage/WriteMessage.tsx
+++ src/components/WriteMessage/WriteMessage.tsx
@@ -1,31 +1,63 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.scss';
 import { useWithWhoChatStore } from '../../stores/withWhoChat';
 export default function WriteMessage() {
     const [messageText, changeMessageText] = useState('');
     const withWhoChat = useWithWhoChatStore((state) => state.withWhoChat);
-    const sendMessage = () => {
-        fetch('api/sendMessage', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                withWhoChat: withWhoChat,
-                messageText: messageText,
-            }),
-        });
+    const [errorTextarea, changeErrorTextarea] = useState(false);
+
+    const sendMessage = async () => {
+        if (!messageText.length) {
+            changeErrorTextarea(true);
+            setTimeout(() => {
+                changeErrorTextarea(false);
+            }, 4000);
+
+            return false;
+        }
+        try {
+            const response = await fetch('api/sendMessage', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    withWhoChat: withWhoChat,
+                    messageText: messageText,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Error of fetch: ' + response.status);
+            }
+
+            changeMessageText('');
+        } catch (error) {
+            console.log('Cannot send message, error -> ', error);
+        }
     };
 
+    const listenKeyboardKeys = (
+        event: React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            sendMessage();
+        }
+    };
     return (
         <>
             <div className='block-write-messages'>
                 <textarea
-                    className='input-write-message'
-                    id='input-write-message'
+                    className={`${
+                        errorTextarea === true ? 'error ' : ''
+                    }block-write-messages__textarea`}
+                    id='block-write-messages__textarea'
                     placeholder='Введите сообщение...'
+                    value={messageText}
                     onChange={(e) => changeMessageText(e.target.value)}
+                    onKeyDown={(e) => listenKeyboardKeys(e)}
                     maxLength={1000}
                 />
                 <button
