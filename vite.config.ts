0ff00afc7c156b6ddc7decc82db5d6ef4bf92a33
Amogus <empty>
2025-10-02 16:10:38 +0400

    5.3-remove_custom_cert

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-// https://vite.dev/config/
+
 export default defineConfig({
     plugins: [react()],
     server: {
-        https: {
-            key: fs.readFileSync('./cert/localhost-key.pem'),
-            cert: fs.readFileSync('./cert/localhost.pem'),
-        },
         host: 'localhost',
         port: 5173,
         proxy: {
